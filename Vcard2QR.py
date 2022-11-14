7ae01efe1e946862f8c5adeca9a9e145c2768c1e
KaO <thierryoliv@gmail.com>
2022-11-14 14:00:49 +0100

    Vcard

---
+++ Vcard2QR.py
@@ -0,0 +1,43 @@
+import qrcode
+
+# write all information
+name = ""
+last_name = ""
+title = ""
+organization = ""
+url = ""
+email = ""
+cellphone = ""
+adresse = ""
+fill_color = "black"
+back_color = "white"
+
+# create information string
+qrcode_info = f"""
+BEGIN:VCARD
+VERSION:3.0
+N:{last_name}; {name}
+FN:{name} {last_name}
+TITLE:{title}
+ORG:{organization}
+URL:{url}
+ADR;WORK;PREF:;;{adresse}
+EMAIL;PREF;INTERNET:{email}
+TEL;TYPE=voice,cell,pref:{cellphone}
+END:VCARD
+"""
+
+# create qrcode instance
+qrcode = qrcode.QRCode(
+    error_correction=qrcode.constants.ERROR_CORRECT_H,
+    box_size=10,
+    border=4,
+)
+
+# add data to the instance and render it
+qrcode.add_data(qrcode_info)
+qrcode.make(fit=True)
+
+# create the image and save it
+img = qrcode.make_image(fill_color=fill_color, back_color=back_color)
+img.save(f"{name}{last_name}.png")
